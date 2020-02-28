b044370396df374e62166209dc832cc3791d1610
Mark Stone <markwstone85@gmail.com>
2020-02-28 15:45:51 +0000

    Automatically backed up by Learn

---
+++ variation-on-wdeloop.rb
@@ -0,0 +1,18 @@
+#3.times do 
+ # puts "I ran."
+#end
+
+#count = 0 
+#while count < 3 do
+ # puts "#{count}"
+  #count = count + 1
+#end 
+
+temperature = 70 
+if temperature <= 50 
+  puts "Bundle up!"
+  elsif (temperature > 50 && temperature < 70)
+  puts "Hoodie weather!"
+else
+  puts "It's a scorcher!"
+end
